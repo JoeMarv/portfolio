b0c9b72cce6aecc1f93d632929c71383432356b8
Joel <joepotential@gmail.com>
2022-07-22 18:38:59 +0100

    template done

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const showBtn = document.querySelector('.show')
 const hideBtn = document.querySelector('.hide')
+const topBtn = document.querySelector ('.top-btn')
 
 showBtn.addEventListener('click', function() {
     const main = showBtn.parentElement.parentElement
@@ -11,4 +12,21 @@ hideBtn.addEventListener('click', function() {
     const main = showBtn.parentElement.parentElement
 
     main.classList.remove('clear')
+})
+
+
+window.addEventListener('scroll', function() {
+    const scrollHeight = window.pageYOffset
+
+    if (scrollHeight > 400) {
+        topBtn.classList.add('show')
+    } 
+    else {
+        topBtn.classList.remove('show')
+    }
+})
+
+topBtn.addEventListener('click', function() {
+    document.body.scrollTop = 0
+    document.documentElement.scrollTop = 0
 })
